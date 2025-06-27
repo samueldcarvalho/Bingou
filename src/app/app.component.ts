2d51a7ffc0f68aa83e6d729ffe810d7b94bcd237
Samuel de Carvalho <samueldcarvalho99@gmail.com>
2025-06-27 00:12:10 -0300

    feat: init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: false,
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'Bingou';
+  protected lastNumberDrawn = signal<string | null>(null);
+
+  protected numbers = Array.from({ length: 100 }, (_, i) => ({
+    num: i.toString(),
+    isDrawn: false,
+  }));
+
+  protected drawNumber(event: any, input: HTMLInputElement) {
+    if (event.key != 'Enter') {
+      return;
+    }
+
+    var selectedNumber = this.numbers.find(
+      (p) => p.num == input.value && p.isDrawn == false,
+    );
+
+    input.value = '';
+
+    if (selectedNumber == null) {
+      return;
+    }
+
+    selectedNumber.isDrawn = true;
+    this.lastNumberDrawn.set(selectedNumber.num);
+  }
 }
