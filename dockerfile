86cac15b5c5c9b250fa83bb06286c16a3d353fe7
Samuel de Carvalho <samueldcarvalho99@gmail.com>
2025-06-27 00:25:52 -0300

    docker: implements dockerfile

---
+++ dockerfile
@@ -0,0 +1,22 @@
+FROM node:20 AS builder
+
+WORKDIR /app
+
+COPY package*.json ./
+RUN npm install
+
+COPY . .
+
+RUN npm run build -- --configuration=production
+
+FROM nginx:alpine
+
+RUN rm -rf /usr/share/nginx/html/*
+
+COPY --from=builder /app/dist/bingou /usr/share/nginx/html
+
+COPY nginx.conf /etc/nginx/conf.d/default.conf
+
+EXPOSE 80
+
+CMD ["nginx", "-g", "daemon off;"]
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+
+const lastNumberKey = 'LAST_NUMBER';
+const numbersKey = 'NUMBERS';
 
 @Component({
   selector: 'app-root',
@@ -6,8 +9,20 @@ import { Component, signal } from '@angular/core';
   standalone: false,
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   protected lastNumberDrawn = signal<string | null>(null);
+
+  ngOnInit(): void {
+    const lastNumber = localStorage.getItem(lastNumberKey);
+    const numbers = localStorage.getItem(numbersKey);
+
+    if ((!!numbers && !!lastNumber) == false) {
+      return;
+    }
+
+    this.numbers = JSON.parse(numbers!);
+    this.lastNumberDrawn.set(lastNumber);
+  }
 
   protected numbers = Array.from({ length: 100 }, (_, i) => ({
     num: i.toString(),
@@ -31,5 +46,19 @@ export class AppComponent {
 
     selectedNumber.isDrawn = true;
     this.lastNumberDrawn.set(selectedNumber.num);
+
+    localStorage.setItem(lastNumberKey, selectedNumber.num);
+    localStorage.setItem(numbersKey, JSON.stringify(this.numbers));
+  }
+
+  protected clear() {
+    localStorage.clear();
+
+    this.numbers = Array.from({ length: 100 }, (_, i) => ({
+      num: i.toString(),
+      isDrawn: false,
+    }));
+
+    this.lastNumberDrawn.set(null);
   }
 }
